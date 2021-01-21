1fa59f402939ccd76064b52d409788a936624db4
hotcakedev628 <hotcake.dev.628@protonmail.com>
2021-01-21 09:06:12 -0700

    import category list to main layout

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -1,26 +1,13 @@
-import React, {
-  useEffect
-} from 'react';
-import { useDispatch } from '../../store';
-import {
-  getCategories
-} from '../../slices/categories';
+import React from 'react';
 import CategoryList from '../../views/categories/CategoryList';
-// import CategoryView from '../../views/categories/CategoryView';
 
 const MainLayout = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getCategories());
-  }, [dispatch]);
 
   return (
     <div
       className="main-layout"
     >
       <CategoryList />
-      {/* <CategoryView /> */}
     </div>
   );
 }
